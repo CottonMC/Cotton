4c4a8364c61d2c4c17ace3c34a06f81780b0b7f7
Jakob <jacky2611@gmail.com>
2018-12-28 22:09:47 +0100

    Removed a method and change ItemSetting to BlockItem

--- src/main/java/io/github/cottonmc/cotton/registry/CommonBlocks.java
+++ src/main/java/io/github/cottonmc/cotton/registry/CommonBlocks.java
@@ -2,6 +2,7 @@ package io.github.cottonmc.cotton.registry;
 
 import io.github.cottonmc.cotton.Cotton;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.block.BlockItem;
@@ -11,55 +12,42 @@ import net.minecraft.util.registry.Registry;
 public class CommonBlocks {
 	public static final String SHARED_NAMESPACE = "cotton";
 
-	/**Attempts to get a common block by name. If no block with this name was found register the given block and return it.
+	/**Attempts to get a common block by name. If no block with this name was found register the given block and create a BlockItem for it.
 	 * @param name The block name to look for. This is the path and does not include the namespace.
 	 * @param block A block that will be registered if no block with this name was found.
 	 * @return Returns either an already existing block with the specified name or a new one that was register under the given name.
 	 */
 	public static Block register(String name, Block block) {
-		return register(name, block, Cotton.commonGroup);
+		BlockItem item = new BlockItem(block, (new Item.Settings()).itemGroup(Cotton.commonGroup));
+		return register(name, block, item);
 	}
 
-	/**Attempts to get a common block by name. If no block with this name was found register the given block, add it to the specified tab
-	 * and return it.
-	 * Only use this method if you are sure that you want to add the new block to a specific tab.
+	/**Attempts to get a common block by name. If no block with this name was found register the given block and the provided BlockItem.
 	 *
 	 * @param name The block name to look for. This is the path and does not include the namespace.
 	 * @param block A block that will be registered if no block with this name was found.
-	 * @param tab The tab the new block will be added to.
+	 * @param item The settings of the new Blocks BlockItem. The tab will be changed to the cotton default tab.
 	 * @return Returns either an already existing block with the specified name or a new one that was register under the given name.
 	 */
-	public static Block register(String name, Block block, ItemGroup tab) {
-		return register(name, block, new Item.Settings(), tab);
+	public static Block register(String name, Block block, BlockItem item) {
+		return register(name, block, item, Cotton.commonGroup);
 	}
 
-	/**Attempts to get a common block by name. If no block with this name was found register the given block.
-	 *
-	 * @param name The block name to look for. This is the path and does not include the namespace.
-	 * @param block A block that will be registered if no block with this name was found.
-	 * @param settings The settings of the new Blocks BlockItem. The tab will be changed to the cotton default tab.
-	 * @return Returns either an already existing block with the specified name or a new one that was register under the given name.
-	 */
-	public static Block register(String name, Block block, Item.Settings settings) {
-		return register(name, block, settings, Cotton.commonGroup);
-	}
-
-	/**Attempts to get a common block by name. If no block with this name was found register the given block, add it to the specified tab
-	 * and return it.
+	/**Attempts to get a common block by name. If no block with this name was found register the given block and BlockItem,
+	 * add it to the specified tab and return the Block.
 	 * Only use this method if you are sure that you want to add the new block to a tag.
 	 *
 	 * @param name The block name to look for. This is the path and does not include the namespace.
 	 * @param block A block that will be registered if no block with this name was found.
- 	 * @param settings The settings of the blocks BlockItem.
-	 * @param tab The tab the new block will be added to.
+ 	 * @param item The BlockItem that will be added should no Block with that name exist.
+	 * @param tab The tab the new BlockItem will be added to.
 	 * @return Returns either an already existing block with the specified name or a new one that was register under the given name.
 	 */
-	public static Block register(String name, Block block, Item.Settings settings, ItemGroup tab) {
+	public static Block register(String name, Block block, BlockItem item, ItemGroup tab) {
 		Identifier id = new Identifier(SHARED_NAMESPACE, name);
 		if (!Registry.BLOCK.contains(id)) {
 			Registry.register(Registry.BLOCK, id, block);
-			BlockItem item = new BlockItem(block, settings.itemGroup(tab));
-			CommonItems.register(id.getPath(), item);
+			CommonItems.register(id.getPath(), item, tab);
 			return block;
 		}
 		else {
