6746ec2405c70fa92e8160b726c2c31d2955e7c9
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-09 23:42:55 +0000

    - simplified implementation of dispenser placing

--- src/main/java/io/github/cottonmc/cotton/mixin/DispenserBlockMixin.java
+++ src/main/java/io/github/cottonmc/cotton/mixin/DispenserBlockMixin.java
@@ -5,21 +5,20 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.DispenserBehavior;
 import net.minecraft.item.Item;
+import net.minecraft.util.SystemUtil;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
+
+import java.util.Map;
 
 @Mixin(DispenserBlock.class)
 public abstract class DispenserBlockMixin {
 
-    /**
-     * This is used to replace the default behavior with block placing.
-     *
-     * @author Juuz
-     */
-    // method_10008 is the lambda in the static initializer
-    @SuppressWarnings("OverwriteTarget")
-    @Overwrite
-    private static void method_10008(Object2ObjectOpenHashMap<Item, DispenserBehavior> map) {
-        map.defaultReturnValue(new PlaceBlockBehavior());
-    }
+    @Mutable
+    @Shadow @Final
+    private static Map<Item, DispenserBehavior> BEHAVIORS = SystemUtil.consume(new Object2ObjectOpenHashMap<>(), (object2ObjectOpenHashMap_1) -> {
+        object2ObjectOpenHashMap_1.defaultReturnValue(new PlaceBlockBehavior());
+    });
 }
