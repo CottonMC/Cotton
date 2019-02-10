031d60ea5fe7317d8ea93f2322a9b41d5d142de1
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-10 12:32:59 +0000

    - Brute-force impl committed

--- src/main/java/io/github/cottonmc/cotton/mixin/MainMenuScreenMixin.java
+++ src/main/java/io/github/cottonmc/cotton/mixin/MainMenuScreenMixin.java
@@ -6,7 +6,6 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.MainMenuScreen;
 import net.minecraft.client.gui.Screen;
 import net.minecraft.util.Identifier;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,19 +17,18 @@ import java.util.Random;
 @Environment(EnvType.CLIENT)
 @Mixin(MainMenuScreen.class)
 public abstract class MainMenuScreenMixin extends Screen {
-    //TODO: splash loading changed in the newest snapshot
-//    @Shadow @Final
-//    private static Random RANDOM;
-//    
-//    @Shadow
-//    private String splashText;
-//
-//    @Shadow @Final
-//    private static Identifier SPLASHES_LOC;
-//
-//    @Inject(method = "<init>", at = @At("RETURN"))
-//    private void init(CallbackInfo ci) {
-//        Splashes.loadDefaultSplashes(SPLASHES_LOC);
-//        splashText = Splashes.getRandomSplash(RANDOM);
-//    }
+    //TODO: This is a brute-force approach bypassing vanilla logic. Please do this better in the future.
+    // (I am in hurry right now, need to finish this to get it working // Pannoniae
+    private static Random RANDOM = new Random();
+
+    @Shadow
+    private String splashText;
+
+    private static Identifier SPLASHES_LOC = new Identifier("texts/splashes.txt"); // TODO get this value from MinecraftClient
+
+    @Inject(method = "onInitialized", at = @At("RETURN"))
+    private void init(CallbackInfo ci) {
+        Splashes.loadDefaultSplashes(SPLASHES_LOC);
+        splashText = Splashes.getRandomSplash(RANDOM);
+    }
 }
