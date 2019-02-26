52b28765107d5bb87b459729228b495f5f041745
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-26 23:54:46 +0000

    - fixed namespace

--- src/main/java/io/github/cottonmc/cotton/registry/CommonBlocks.java
+++ src/main/java/io/github/cottonmc/cotton/registry/CommonBlocks.java
@@ -3,19 +3,14 @@ package io.github.cottonmc.cotton.registry;
 import io.github.cottonmc.cotton.Cotton;
 import io.github.cottonmc.cotton.tags.TagEntryManager;
 import io.github.cottonmc.cotton.tags.TagType;
-import net.fabricmc.loader.FabricLoader;
-import net.fabricmc.loader.ModContainer;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.block.BlockItem;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
-import java.util.Iterator;
-
 public class CommonBlocks {
-	public static final String SHARED_NAMESPACE = "cotton";
+	public static final String SHARED_NAMESPACE = "c";
 
 	/**Attempts to get a common block by name. If no block with this name was found register the given block and create a BlockItem for it.
 	 * @param name The block name to look for. This is the path and does not include the namespace.
