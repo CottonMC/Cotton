55c7e4685cf1af8995904d49b79cc2d0a62c9a57
Pannoniae <prisznyak.zsombor@gmail.com>
2019-02-26 22:56:15 +0000

    - removed deprecated APIs

--- src/main/java/io/github/cottonmc/cotton/Cotton.java
+++ src/main/java/io/github/cottonmc/cotton/Cotton.java
@@ -18,9 +18,9 @@ import java.io.File;
 public class Cotton implements ModInitializer {
 
 	public static final String MODID = "cotton";
-	public static boolean isDevEnv = Boolean.parseBoolean(System.getProperty("fabric.development", "false"));
+	public static final boolean isDevEnv = Boolean.parseBoolean(System.getProperty("fabric.development", "false"));
 
-	public static File DATA_PACK_LOCATION = new File(FabricLoader.getInstance().getGameDirectory(), "datapacks/cotton (generated)/");
+	public static final File DATA_PACK_LOCATION = new File(FabricLoader.getInstance().getGameDirectory(), "datapacks/cotton (generated)/");
 	public static File getTagLocation(Identifier tagId) {
 		return new File(DATA_PACK_LOCATION, "data/" + tagId.getNamespace() + "/tags");
 	}
@@ -28,16 +28,16 @@ public class Cotton implements ModInitializer {
 	public static final ItemGroup commonGroup = FabricItemGroupBuilder.build(
 			new Identifier("cotton", "common_tab"), () -> new ItemStack(Blocks.LIGHT_BLUE_WOOL));
 
-	public static ModLogger logger = new ModLogger(MODID, "COTTON");
+	public static final ModLogger logger = new ModLogger(MODID, "COTTON");
 	public static CottonConfig config;
 
 	@Override
 	public void onInitialize() {
-		logger.setPrefixFormat(Ansi.Green);
+		logger.setPrefixFormat(Ansi.Blue);
 
 		//example config and logger code
 		config = ConfigManager.loadConfig(CottonConfig.class);
-		logger.info("loaded config");
+		logger.info("Loaded config.");
 		PackMetaManager.saveMeta();
 		//TagEntryManager.registerToTag(TagType.BLOCK, new Identifier("minecraft:enderman_holdable"), "minecraft:string");
 		//TagEntryManager.registerToTag(TagType.BLOCK, new Identifier("minecraft:dragon_immune"), "#minecraft:enderman_holdable");
